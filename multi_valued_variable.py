05f537865cbda34d267a366a930a98035e1c67df
Md Foysal <mdfoysal.cysec@gmail.com>
2023-10-31 02:17:16 +0600

    Commit 1

---
+++ multi_valued_variable.py
@@ -0,0 +1,22 @@
+name1, name2, name3 = 'Md Foysal','Monir','Durjoy'
+
+print(name3)
+print(name1)
+print(name2)
+
+print("")
+
+print(name1,name2,name3)
+
+print("")
+
+students = ['Md Foysal', 'Durjoy', 'Monir', 'Nahid', 'Mahbub']
+a, b, c, d, e = students
+print(a,b,c,d,e, a, a, a)
+
+print("")
+
+a = b = c = 'They are students'
+print(a)
+print(b)
+print(c)
